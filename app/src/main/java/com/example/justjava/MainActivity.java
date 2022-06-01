401716fcd5c740047e7b1ea4af061ca629aa3466
Ishan-Rawat <rawatishan03@gmail.com>
2022-06-01 18:35:25 +0530

    Made app vertically scrollable and added whipped cream to the menu

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -17,11 +17,36 @@
  */
 package com.example.justjava;
 
+import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * An android app is made up of 2 things: Resources(res directory) and java code(java directory)
+ * Resources contain things like texts, layouts, images, fonts, etc)
+ * and java code handles things like what to do when a button is pressed, etc.
+ * This division helps when we want to change the front-end without changing the back end.
+ *
+ * HOW ARE RESOURCES ACCESSED?
+ * when an android app is compiled a tool called aapt creates a java class called R. This contains the resource IDs for all
+ * the resources contained in the res directory.
+ * The resource IDs have the following format:
+ * For java code: R.resource_type.resource_name (for example R.string.hello)
+ * For XML code: @resource_type/resource_name (for ex: @string.hello)
+ *
+ * HOW DO XML AND JAVA WORK TOGETHER?
+ * A lot of the initialisation work for the android app is handles automatically by android. What we need to pay attention to is:
+ * The MainActivity.java has a method called onCreate which is called when the MainActivity is initialised which calls another method
+ * which sets the views for this main activity. This method is passed the activity_main.xml resource as a =n argument. Then, this XML
+ * file is parsed, and then the view hierarchy is created(which is just a hierarchy of java objects)
+ */
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
 import java.text.NumberFormat;
 
@@ -41,8 +66,10 @@ public class MainActivity extends AppCompatActivity {
      */
     int quantity=2;
     public void submitOrder(View view) {
+        CheckBox checkbox = (CheckBox) findViewById(R.id.whipped_cream_checkbox); // if I put this line at line 97 instead, then the app effin crashes. gotta debug
+        boolean status = checkbox.isChecked();
 
-        displayMessage(generateOrderSummary(calculatePrice()));
+        displayMessage(generateOrderSummary(calculatePrice(), status));
     }
 
     // This is the method for the increment button
@@ -68,14 +95,16 @@ public class MainActivity extends AppCompatActivity {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(message);
     }
+
     private int calculatePrice(){
         return (quantity*5);
     }
 
 
-    private String generateOrderSummary(int price){
+    private String generateOrderSummary(int price, boolean checkboxstatus){
         String message = "Name: Shiro T Poison";
         message += "\nQuantity: "+ quantity;
+        message += "\n add whipped cream?: " + checkboxstatus;
         message += "\nPrice: $" + price;
         message += "\nThank You!";
         return message;
